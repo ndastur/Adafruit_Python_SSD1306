d9ca52182b98e175ef30651b7126891016a5554f
Neville Dastur <neville@dastur.me.uk>
2020-11-22 00:38:19 +0000

    Convert to string in the process.
    In the previous implementation of doing the conversion afterwards the output contained non alpha characters converted to \n for instance. This now displays properly

--- examples/stats.py
+++ examples/stats.py
@@ -109,21 +109,21 @@ while True:
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
+    IP = subprocess.check_output(cmd, shell = True, universal_newlines=True )
     cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
+    CPU = subprocess.check_output(cmd, shell = True, universal_newlines=True )
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
+    MemUsage = subprocess.check_output(cmd, shell = True, universal_newlines=True )
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
-
+    Disk = subprocess.check_output(cmd, shell = True, universal_newlines=True )
+    
     # Write two lines of text.
 
-    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
-
+    draw.text((x, top),       "IP: " + IP,  font=font, fill=255)
+    draw.text((x, top+8),     CPU, font=font, fill=255)
+    draw.text((x, top+16),    MemUsage,  font=font, fill=255)
+    draw.text((x, top+25),    Disk,  font=font, fill=255)
+    
     # Display image.
     disp.image(image)
     disp.display()
